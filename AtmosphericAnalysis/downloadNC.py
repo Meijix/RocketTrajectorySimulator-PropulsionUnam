0f836159ec4d2de9436b9188c3e979b891ae4dff
Meijix <nmejia30@ciencias.unam.mx>
2024-07-21 17:50:43 -0600

    Change downloadNC.py file

--- AtmosphericAnalysis/downloadNC.py
+++ AtmosphericAnalysis/downloadNC.py
@@ -1,32 +1,26 @@
-# Install and load required packages
-if(!require("RNCEP")) install.packages("RNCEP")
-if(!require("lubridate")) install.packages("lubridate")
-if(!require("tidyverse")) install.packages("tidyverse")
-if(!require("sf")) install.packages("sf")
+import cdsapi
 
-library(RNCEP)
-library(lubridate)
-library(tidyverse)
-library(sf)
+c = cdsapi.Client()
 
-# Define the necessary arguments
-month_range <- c(1,12)     # period of months
-year_range <- c(2023,2023) # period of years
-
-lat_range <- c(-120, -60)  # latitude range for Mexico and United States
-lon_range <- c(-170, -60)  # longitude range for Mexico and United States
-
-# Download wind data (uwnd and vwnd) for 2023
-data_uwnd <- NCEP.gather("uwnd",    # name of the variable
-                         850, # pressure level 850hPa
-                         month_range, year_range,
-                         lat_range, lon_range,
-                         return.units = TRUE,
-                         reanalysis2=TRUE)
-
-data_vwnd <- NCEP.gather("vwnd",    # name of the variable
-                         850, # pressure level 850hPa
-                         month_range, year_range,
-                         lat_range, lon_range,
-                         return.units = TRUE,
-                         reanalysis2=TRUE)
+c.retrieve(
+    'reanalysis-era5-single-levels-monthly-means',
+    {
+        'product_type': 'monthly_averaged_reanalysis',
+        'variable': [
+            '100m_u_component_of_wind', '100m_v_component_of_wind', '10m_u_component_of_neutral_wind',
+            '10m_u_component_of_wind', '10m_v_component_of_neutral_wind', '10m_v_component_of_wind',
+            '10m_wind_speed', 'instantaneous_10m_wind_gust',
+        ],
+        'year': [
+            '2022', '2023', '2024',
+        ],
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+        ],
+        'time': '00:00',
+        'format': 'netcdf',
+    },
+    'download.nc')
