5fe27f705b6b0c12fcd192d650952b4eea1989ea
Meijix <nmejia30@ciencias.unam.mx>
2024-07-25 15:10:45 -0600

    Add flight data analysis files
    Se agregan archivos para graficar y analizar datos de vuelo de la ASYB Y GPSFeatherweight

---
+++ Extras/analizar_FW.py
@@ -0,0 +1,39 @@
+import datetime as dtm
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.signal import savgol_filter
+
+df = pd.read_csv("Copia de FthrWtTRAKR_06-22-2024_12_37_13.csv")
+
+ifirst = 934
+t_apogeo = 43
+FT_TO_M = 0.3048
+
+tiempo_np = pd.to_datetime(df["TIME"], format="%H:%M:%S.%f").to_numpy()
+tiempo = np.array([(tiempo_np[i]-tiempo_np[ifirst])/np.timedelta64(1,'s') for i in range(len(tiempo_np))])
+
+tiempo = tiempo[ifirst:]
+vertv = df.iloc[ifirst:]["VERTV"]*FT_TO_M
+
+
+# plt.plot(tiempo[1:]-tiempo[:-1])
+# plt.show()
+
+plt.figure()
+plt.plot(tiempo, vertv)
+plt.title("Velocidad vertical")
+plt.xlabel('Tiempo [s]')
+plt.ylabel('m/s')
+plt.axvline(t_apogeo, ls="--")
+
+plt.figure()
+vertacc = np.gradient(vertv)
+plt.plot(vertacc)
+plt.title("Aceleración vertical")
+plt.ylabel('m/s^2')
+plt.axvline(t_apogeo, ls="--")
+plt.axhline(-9.8, ls="-", color="gray")
+
+plt.show()
+
---
+++ Extras/analizar_vueloASYB.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.signal import savgol_filter
+
+df = pd.read_csv("Copia de vueloSpaceport.csv")
+
+diffs = df["time"].to_numpy()[1:]-df["time"].to_numpy()[:-1]
+plt.plot(diffs)
+plt.show()
+
+tiempo = df["time"]/1e3
+altitud = df["altitude"]
+altitud_suave = savgol_filter(altitud, 25, 13)
+ax = df["ax"]
+ay = df["ay"]
+az = df["az"]
+atot = np.sqrt(ax**2+ay**2+az**2)
+
+# plt.plot(tiempo, altitud, label="Cruda")
+# plt.plot(tiempo, altitud_suave, label="Suavizada")
+# plt.legend()
+plt.show()
+
+# plt.plot(tiempo, ax, label="ax")
+# plt.plot(tiempo, ay, label="ay")
+# plt.plot(tiempo, az, label="az")
+# plt.plot(tiempo, atot, label="tot")
+# plt.legend()
+
+
+
+vert_vel = np.gradient(altitud, 0.2)
+# vert_vel2 = np.gradient(altitud_suave, 0.2)
+plt.figure()
+
+plt.plot(tiempo, vert_vel)
+plt.plot(tiempo, savgol_filter(vert_vel, 25, 1))
+# plt.plot(tiempo, vert_vel2)
+
+
+
+vert_acc = np.gradient(savgol_filter(vert_vel, 25, 5))
+plt.figure()
+plt.title("Aceleración vertical")
+plt.plot(tiempo, vert_acc)
+
+plt.show()
+
+
