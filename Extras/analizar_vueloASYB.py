efba33e7cf7ceef46730087f1fffaadbfadfe3fb
Meijix <nmejia30@ciencias.unam.mx>
2024-07-31 10:29:48 -0600

    Improve ASYB data analysis
    Equiespaciamiento de los datos obtenidos para hacer diferenciacion numerica

--- Extras/analizar_vueloASYB.py
+++ Extras/analizar_vueloASYB.py
@@ -5,21 +5,43 @@ from scipy.signal import savgol_filter
 
 df = pd.read_csv(r'C:\Users\Natalia\OneDrive\Tesis\GithubCode\3DOF-Rocket-PU\Archivos\Copia de vueloSpaceport.csv')
 
+#pasos de tiempo en milisegundos entre cada recepcion de datos
 diffs = df["time"].to_numpy()[1:]-df["time"].to_numpy()[:-1]
 plt.plot(diffs)
+plt.title("Variacion del paso en datos recolectados")
 plt.show()
 
+#datos obtenidos
 tiempo = df["time"]/1e3
 altitud = df["altitude"]
 altitud_suave = savgol_filter(altitud, 25, 13)
+
+#Equiespaciamiento de datos
+def calc_dato(t):
+  if t > tiempo.max():
+    return 0
+  else:
+    # Realizar interpolación
+    return np.interp(t, tiempo, altitud)
+
+#cant=tiempo.max()/0.2
+#cant=int(cant)
+#print(cant)
+#tiempo_equiesp=np.linspace(0,3740,cant)
+time_equiesp=np.linspace(3676,3739,2000)
+altura_interp = [calc_dato(t) for t in time_equiesp]
+
+
+#aceleraciones obtenidas
 ax = df["ax"]
 ay = df["ay"]
 az = df["az"]
 atot = np.sqrt(ax**2+ay**2+az**2)
 
-# plt.plot(tiempo, altitud, label="Cruda")
-# plt.plot(tiempo, altitud_suave, label="Suavizada")
-# plt.legend()
+plt.plot(tiempo, altitud, label="Cruda")
+#plt.plot(tiempo, altitud_suave, label="Suavizada")
+plt.plot(time_equiesp,altura_interp, label= "Interpolacion")
+plt.legend()
 plt.show()
 
 # plt.plot(tiempo, ax, label="ax")
@@ -32,19 +54,34 @@ plt.show()
 
 vert_vel = np.gradient(altitud, 0.2)
 # vert_vel2 = np.gradient(altitud_suave, 0.2)
+vert_vel3 = np.gradient(altura_interp, 0.2)
 plt.figure()
 
-plt.plot(tiempo, vert_vel)
-plt.plot(tiempo, savgol_filter(vert_vel, 25, 1))
-# plt.plot(tiempo, vert_vel2)
+plt.plot(tiempo, vert_vel, label="De datos")
+#plt.plot(tiempo, savgol_filter(vert_vel, 25, 1))
+plt.plot(time_equiesp, vert_vel3, label="De equiespaciamiento")
+plt.title("Velocidad vertical numerica")
+plt.legend()
+plt.show()
 
 
 
 vert_acc = np.gradient(savgol_filter(vert_vel, 25, 5))
+#plt.figure()
+#plt.title("Aceleración vertical")
+#plt.plot(tiempo, vert_acc)
+
+#plt.show()
+
+acc_vel = np.gradient(vert_vel, 0.2)
+# vert_vel2 = np.gradient(altitud_suave, 0.2)
+acc_vel3 = np.gradient(vert_vel3, 0.2)
 plt.figure()
-plt.title("Aceleración vertical")
-plt.plot(tiempo, vert_acc)
 
+plt.plot(tiempo, acc_vel, label="De datos")
+#plt.plot(tiempo, savgol_filter(vert_vel, 25, 1))
+plt.plot(time_equiesp, acc_vel3, label="De equiespaciamiento")
+plt.title("Aceleracion vertical numerica")
+plt.legend()
 plt.show()
-
 
