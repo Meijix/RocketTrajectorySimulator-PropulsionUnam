af16d397b9145a9b9909be653caa651808896609
Meijix <nmejia30@ciencias.unam.mx>
2024-08-19 09:56:19 -0600

    Improve riel.py

--- Simulador/riel.py
+++ Simulador/riel.py
@@ -6,7 +6,10 @@ class Torrelanzamiento:
     self.longitud = longitud #[m]
     self.angulo = angulo #[grados]
 
-# riel = Torrelanzamiento(5, 88)
-# riel = Torrelanzamiento(5, 45)
-riel = Torrelanzamiento(10, 80)
-print("Riel de lanzamiento:",riel.longitud,"m", riel.angulo, "º")
+
+if __name__ == "__main__":
+  #riel = Torrelanzamiento(5, 88)
+  #riel = Torrelanzamiento(5, 45)
+  riel = Torrelanzamiento(10, 80)
+  print("Riel de lanzamiento:",riel.longitud,"m", riel.angulo, "º")
+  
