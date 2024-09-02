6600e1e53d9ed4c8ee623a3715762bf08b79bd10
Meijix <nmejia30@ciencias.unam.mx>
2024-09-02 10:45:03 -0600

    Add funciones extraer_datos
    Se agregan funciones que extraen todos los datos del archivo csv y otras del archivo json. Para multiples simulaciones poder extraer datos de diferentes archivos y graficar juntos.

--- Simulador/Graficas.py
+++ Simulador/Graficas.py
@@ -12,79 +12,98 @@ from dibujarCohete import *
 #vuelo_graficar=vuelo1
 #vuelo_graficar=vuelo_paracaidas
 
+def extraer_datoscsv(datos_simulacion):
+    # Convertir los datos a arrays de numpy
+    tiempos = datos_simulacion['tiempos'].values
+
+    posiciones = datos_simulacion[['posiciones_x', 'posiciones_y', 'posiciones_z']].values
+    velocidades = datos_simulacion[['velocidades_x', 'velocidades_y', 'velocidades_z']].values
+    thetas = datos_simulacion['thetas'].values
+    omegas = datos_simulacion['omegas'].values
+
+    CPs = datos_simulacion['CPs'].values
+    CGs = datos_simulacion['CGs'].values
+    masavuelo = datos_simulacion['masavuelo'].values
+    estabilidad = datos_simulacion['estabilidad'].values
+
+    viento_vuelo_mags = datos_simulacion['viento_vuelo_mags'].values
+    viento_vuelo_dirs = datos_simulacion['viento_vuelo_dirs'].values
+    viento_vuelo_vecs = datos_simulacion['viento_vuelo_vecs'].values
+    wind_xs = datos_simulacion['wind_xs'].values
+    wind_ys = datos_simulacion['wind_ys'].values
+    wind_zs = datos_simulacion['wind_zs'].values
+
+    Tmags = datos_simulacion['Tmags'].values
+    Dmags = datos_simulacion['Dmags'].values
+    Nmags = datos_simulacion['Nmags'].values
+    Txs = datos_simulacion['Txs'].values
+    Tys = datos_simulacion['Tys'].values
+    Tzs = datos_simulacion['Tzs'].values
+    Dxs = datos_simulacion['Dxs'].values
+    Dys = datos_simulacion['Dys'].values
+    Dzs = datos_simulacion['Dzs'].values
+    Nxs = datos_simulacion['Nxs'].values
+    Nys = datos_simulacion['Nys'].values
+    Nzs = datos_simulacion['Nzs'].values
+
+    accels = datos_simulacion['accels'].values
+    palancas = datos_simulacion['palancas'].values
+    accangs = datos_simulacion['accangs'].values
+
+    Gammas = datos_simulacion['Gammas'].values
+    Alphas = datos_simulacion['Alphas'].values
+    torcas = datos_simulacion['torcas'].values
+
+    Cds = datos_simulacion['Cds'].values
+    Machs = datos_simulacion['Machs'].values
+    #########################################
+    #Obtener magnitudes y separar coordenadas
+    Tvecs = np.column_stack((Txs, Tys, Tzs))
+    Dvecs = np.column_stack((Dxs, Dys, Dzs))
+    Nvecs = np.column_stack((Nxs, Nys, Nzs))
+    #print(Tvecs)
+
+    CGs = np.array(CGs)
+    CPs = np.array(CPs)
+    return (tiempos, posiciones, velocidades, thetas, omegas, CPs, CGs, masavuelo,estabilidad,
+            viento_vuelo_mags, viento_vuelo_dirs, viento_vuelo_vecs, wind_xs, wind_ys, wind_zs,
+            Dmags, Nmags, Tmags, Dxs, Dys, Dzs, Nxs, Nys, Nzs, Txs, Tys, Tzs, Tvecs, Dvecs, Nvecs,
+            accels, palancas, accangs, Gammas, Alphas, torcas, Cds, Machs)
+
+def extraer_datosjson(datos):
+    #print(datos)
+    diam_ext = datos['d_ext']
+    t_MECO = datos["t_MECO"]
+    tiempo_salida_riel = datos["tiempo_salida_riel"]
+    tiempo_apogeo = datos["tiempo_apogeo"]
+    tiempo_impacto = datos["tiempo_impacto"]
+    max_altitude = datos["max_altitude"]
+    max_speed = datos["max_speed"]
+    max_acceleration_linear = datos["max_acceleration_linear"]
+    max_acceleration_angular = datos["max_acceleration_angular"]
+    return (diam_ext, t_MECO, tiempo_salida_riel, tiempo_apogeo, tiempo_impacto, 
+            max_altitude, max_speed, max_acceleration_linear, max_acceleration_angular)
+
+
 ###########################################################333
 # Leer los datos de la simulación desde el archivo CSV
 datos_simulacion = pd.read_csv('datos_simulacion.csv')
+# Extarer los datos del csv
+(tiempos, posiciones, velocidades, thetas, omegas, CPs, CGs, masavuelo,estabilidad,
+viento_vuelo_mags, viento_vuelo_dirs, viento_vuelo_vecs, wind_xs, wind_ys, wind_zs,
+Dmags, Nmags, Tmags, Dxs, Dys, Dzs, Nxs, Nys, Nzs, Txs, Tys, Tzs, Tvecs, Dvecs, Nvecs,
+accels, palancas, accangs, Gammas, Alphas, torcas, Cds, Machs) = extraer_datoscsv(datos_simulacion)
 
-# Convertir los datos a arrays de numpy
-tiempos = datos_simulacion['tiempos'].values
-
-posiciones = datos_simulacion[['posiciones_x', 'posiciones_y', 'posiciones_z']].values
-velocidades = datos_simulacion[['velocidades_x', 'velocidades_y', 'velocidades_z']].values
-thetas = datos_simulacion['thetas'].values
-omegas = datos_simulacion['omegas'].values
-
-CPs = datos_simulacion['CPs'].values
-CGs = datos_simulacion['CGs'].values
-masavuelo = datos_simulacion['masavuelo'].values
-estabilidad = datos_simulacion['estabilidad'].values
-
-viento_vuelo_mags = datos_simulacion['viento_vuelo_mags'].values
-viento_vuelo_dirs = datos_simulacion['viento_vuelo_dirs'].values
-viento_vuelo_vecs = datos_simulacion['viento_vuelo_vecs'].values
-wind_xs = datos_simulacion['wind_xs'].values
-wind_ys = datos_simulacion['wind_ys'].values
-wind_zs = datos_simulacion['wind_zs'].values
-
-Tmags = datos_simulacion['Tmags'].values
-Dmags = datos_simulacion['Dmags'].values
-Nmags = datos_simulacion['Nmags'].values
-Txs = datos_simulacion['Txs'].values
-Tys = datos_simulacion['Tys'].values
-Tzs = datos_simulacion['Tzs'].values
-Dxs = datos_simulacion['Dxs'].values
-Dys = datos_simulacion['Dys'].values
-Dzs = datos_simulacion['Dzs'].values
-Nxs = datos_simulacion['Nxs'].values
-Nys = datos_simulacion['Nys'].values
-Nzs = datos_simulacion['Nzs'].values
-
-accels = datos_simulacion['accels'].values
-palancas = datos_simulacion['palancas'].values
-accangs = datos_simulacion['accangs'].values
-
-Gammas = datos_simulacion['Gammas'].values
-Alphas = datos_simulacion['Alphas'].values
-torcas = datos_simulacion['torcas'].values
-
-Cds = datos_simulacion['Cds'].values
-Machs = datos_simulacion['Machs'].values
-#########################################
-#Obtener magnitudes y separar coordenadas
-Tvecs = np.column_stack((Txs, Tys, Tzs))
-Dvecs = np.column_stack((Dxs, Dys, Dzs))
-Nvecs = np.column_stack((Nxs, Nys, Nzs))
-#print(Tvecs)
-
-CGs = np.array(CGs)
-CPs = np.array(CPs)
 #########################################
 # Leer los datos de la simulación desde el archivo JSON
 import json
 
 with open('datos_simulacion.json', 'r') as f:
     datos = json.load(f)
+# Extraer los datos del json
+(d_ext, t_MECO, tiempo_salida_riel, tiempo_apogeo, tiempo_impacto,
+    max_altitude, max_speed, max_acceleration_linear, max_acceleration_angular) = extraer_datosjson(datos)
 
-#print(datos)
-diam_ext = datos['d_ext']
-t_MECO = datos["t_MECO"]
-tiempo_salida_riel = datos["tiempo_salida_riel"]
-tiempo_apogeo = datos["tiempo_apogeo"]
-tiempo_impacto = datos["tiempo_impacto"]
-max_altitude = datos["max_altitude"]
-max_speed = datos["max_speed"]
-max_acceleration_linear = datos["max_acceleration_linear"]
-max_acceleration_angular = datos["max_acceleration_angular"]
 
 ##########################################
 # Funcion para graficar los tiempos importantes
