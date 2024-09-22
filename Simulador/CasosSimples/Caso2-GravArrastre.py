927f0432d156cb8173d7180411413ce3f2d4201f
Meijix <nmejia30@ciencias.unam.mx>
2024-09-22 00:53:01 -0600

    Add varios simuladores a funcion simular

--- Simulador/CasosSimples/Caso2-GravArrastre.py
+++ Simulador/CasosSimples/Caso2-GravArrastre.py
@@ -131,7 +131,7 @@ plt.legend()
 
 plt.show()
 
-
+'''
 #Calcular y graficar el error numerico
 #error en metros
 error_pos = [pos_simul[i] - pos_analitica[i] for i in range(len(tiempos))]
@@ -192,6 +192,117 @@ plt.figure(figsize=(8, 6))
 plt.plot(t_scipy, vel_scipy, label='Scipy')
 plt.plot(tiempos, vel_analitica, label='Analítica')
 plt.plot(tiempos, vel_simul, label='Simulación')
+plt.title('Velocidad vertical [m/s]')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Velocidad [m/s]')
+plt.legend()
+
+plt.show()
+
+'''
+
+
+##############################################################################
+#Comparacion integradores
+# ...
+
+def simular_dinamica(estado, t_max, dt, integrador):
+    # ...
+    sim = [estado]
+    tiempos = [0]
+    t = 0.0
+    it = 1
+
+    Integracion = integrador(der_gravedad_arrastre)
+
+    while t < t_max:
+        nuevo_estado = Integracion.step(t, estado, dt)
+        
+        # Avanzar estado
+        it += 1
+        t += dt
+        estado = nuevo_estado
+
+        sim.append(estado)
+        tiempos.append(t)
+
+        #Indicar el avance en la simulacion
+        if it%500==0:
+            print(f"Iteracion {it}, t={t:.1f} s, altitud={estado[0]:.1f} m, vel vert={estado[1]:.1f}")
+            
+        if estado[0] < 0:
+            break
+
+    return tiempos, sim
+
+# Listas para guardar los resultados
+tiempos_euler = []
+pos_euler = []
+vel_euler = []
+
+tiempos_rk4 = []
+pos_rk4 = []
+vel_rk4 = []
+
+tiempos_rkf45 = []
+pos_rkf45 = []
+vel_rkf45 = []
+
+# Estado inicial
+z0 = 0
+v0 = 100
+estado = np.array([z0, v0])
+
+# Parametros de la simulacion
+dt = 0.1
+t_max = 80
+
+# Simulaciones con diferentes integradores
+integradores = [Euler, RungeKutta4, RungeKutta2]#, RKF45]
+labels = ['Euler', 'RK4', 'RK2'] #, 'RKF45']
+
+for integrador, label in zip(integradores, labels):
+    tiempos, sim = simular_dinamica(estado, t_max, dt, integrador)
+    pos = [sim[i][0] for i in range(len(sim))]
+    vel = [sim[i][1] for i in range(len(sim))]
+    
+    if label == 'Euler':
+        tiempos_euler = tiempos
+        pos_euler = pos
+        vel_euler = vel
+    elif label == 'RK4':
+        tiempos_rk4 = tiempos
+        pos_rk4 = pos
+        vel_rk4 = vel
+    elif label =='RK2':
+        tiempos_rk2 = tiempos
+        pos_rk2 = pos
+        vel_rk2 = vel
+    elif label == 'RKF45':
+        tiempos_rkf45 = tiempos
+        pos_rkf45 = pos
+        vel_rkf45 = vel
+
+opacidad=1
+# Graficar resultados
+plt.figure(figsize=(8, 6))
+#Checar el tamano de la solcion analitica?
+#plt.plot(tiempos, pos_analitica, label='Analitica', ls='-')
+plt.plot(tiempos_euler, pos_euler, label='Euler',marker ='o', alpha=opacidad)
+plt.plot(tiempos_rk4, pos_rk4, label='RK4', marker='*', alpha= opacidad)
+plt.plot(tiempos_rk2, pos_rk2, label='RK2', linestyle='dashed', alpha=opacidad) #marker ='v', alpha= opacidad)
+plt.plot(tiempos_rkf45, pos_rkf45, label='RKF45', marker='X',alpha=opacidad)
+plt.title('Posición vertical [m]')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Posición [m]')
+plt.legend()
+
+plt.figure(figsize=(8, 6))
+#plt.plot(tiempos, vel_analitica, label='Analitica', ls='-')
+plt.plot(tiempos_euler, vel_euler, label='Euler', marker='o')
+plt.plot(tiempos_rk4, vel_rk4, label='RK4', marker='*')
+plt.plot(tiempos_rk2, vel_rk2, label='RK2', linestyle='dashed', alpha=opacidad) 
+plt.plot(tiempos_rkf45, vel_rkf45, label='RKF45',marker='X')
 plt.title('Velocidad vertical [m/s]')
 plt.xlabel('Tiempo [s]')
 plt.ylabel('Velocidad [m/s]')
