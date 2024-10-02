e56652310ba7034e5e7a4619bb90791d92cbeefe
Meijix <nmejia30@ciencias.unam.mx>
2024-10-01 18:25:45 -0600

    Diccionarios  para n dt's distintos Funcional

--- Simulador/CasosSimples/Caso2-Pruebas.py
+++ Simulador/CasosSimples/Caso2-Pruebas.py
@@ -58,6 +58,7 @@ def graficar_resultados(dt_values, resultados, tipo='posición'):
 # Inicialización de parámetros
 Integrador_oficial = RungeKutta2
 dt_values = [0.005, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2]
+dt_values = [0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.125, 0.2]
 resultados = {}
 
 # Simulaciones
