1a9a5dd991461545d64cddfc00f33de1c3a78ee8
Meijix <nmejia30@ciencias.unam.mx>
2024-10-22 13:35:23 -0600

    Mejora dibujo cohete

---
+++ Simulador/__init__.py
@@ -0,0 +1,4 @@
+#Archivo vacio 
+#Este archivo es necesario para que Python trate a la carpeta como un paquete
+#No es necesario que contenga código
+#NO BORRAR
---
+++ Simulador/utils/__init__.py
@@ -0,0 +1,4 @@
+from .dibujar_cohete2 import dibujarCohete
+from .dibujar_cohete2 import dibujarCohete2
+from .angulos import *
+from .funciones import *
