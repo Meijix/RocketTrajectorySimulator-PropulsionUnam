b2ec6f3697bd4d848472f6673058d81e9cb9d392
Meijix <nmejia30@ciencias.unam.mx>
2024-11-11 19:40:45 -0600

    Fix condiciones_init.py

--- Simulador/src/condiciones_init.py
+++ Simulador/src/condiciones_init.py
@@ -1,17 +1,23 @@
 #Agregar condiciones iniciales
 
-from Paquetes.PaqueteFisica.riel import *
-from Paquetes.PaqueteFisica.atmosfera import *
-from Paquetes.PaqueteFisica.viento import *
+import sys
+import os
+
+# Agregar la ruta del directorio que contiene los paquetes al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+#Importar paquetes propios de carpeta superior Paquetes
+from Paquetes.PaqueteFisica.riel import Torrelanzamiento
+from Paquetes.PaqueteFisica.atmosfera import atmosfera
+from Paquetes.PaqueteFisica.viento import Viento
 
 #riel inicial
 riel = Torrelanzamiento(10, 88)
-#lugar de lanzamiento
+""" #lugar de lanzamiento
 latitud_cord = 19.5
 longitud_cord = -98.8
 altitud_cord = 20
 #Indicar fecha actual
-fecha = "2024-11-06"
+fecha = "2024-11-06" """
 
 #viento
 #viento_actual = Viento( 10, 2, 0, 0)
