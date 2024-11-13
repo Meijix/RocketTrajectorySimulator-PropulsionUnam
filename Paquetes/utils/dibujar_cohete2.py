dc6f383dd03b9e16a260f344d9ead79a4b1f83d5
Meijix <nmejia30@ciencias.unam.mx>
2024-11-12 20:59:16 -0600

    Improve dibujo_cohete2

--- Paquetes/utils/dibujar_cohete2.py
+++ Paquetes/utils/dibujar_cohete2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+    
 
 
 def dibujar_cohete2(x_cm=3, y_cm=0.5, edge_color='b', alpha='0.8'):
@@ -13,10 +14,8 @@ def dibujar_cohete2(x_cm=3, y_cm=0.5, edge_color='b', alpha='0.8'):
     edge_color (str): Color del borde (default 'b')
     alpha (str): Transparencia del relleno (default '0.8')
     """
-    import matplotlib.pyplot as plt
-    import matplotlib.patches as patches
-    import numpy as np
-    
+    color_cohete='navy'
+    color_borde='silver'
     # Crear figura
     fig = plt.figure()
     
@@ -33,25 +32,25 @@ def dibujar_cohete2(x_cm=3, y_cm=0.5, edge_color='b', alpha='0.8'):
     
     # Cuerpo del cohete
     points = np.array([(0,0), (0,body_w), (body_l,body_w), (body_l, 0)])
-    body = patches.Polygon(points, facecolor=alpha, edgecolor=edge_color)
+    body = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
     
     # Cono de la nariz
     points = np.array([(body_l,0), (body_l,body_w), (body_l+nose_l,body_w/2)])
-    nose_cone = patches.Polygon(points, facecolor=alpha, edgecolor=edge_color)
+    nose_cone = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
     
     # Aletas
     fin_d = (fin_w2 - fin_w1)/2
     points = np.array([(0, 0), (fin_d, fin_h), (fin_d+fin_w1, fin_h), (fin_w2, 0)])
     points1 = np.copy(points)
     points1[:, 1] += body_w
-    fin1 = patches.Polygon(points1, facecolor=alpha, edgecolor=edge_color)
+    fin1 = patches.Polygon(points1, facecolor=color_cohete, edgecolor=color_borde)
     points2 = np.copy(points)
     points2[:, 1] *= -1
-    fin2 = patches.Polygon(points2, facecolor=alpha, edgecolor=edge_color)
+    fin2 = patches.Polygon(points2, facecolor=color_cohete, edgecolor=color_borde)
     
     # Boattail (parte trasera)
     points = np.array([(0,0), (0,body_w), (-boattail_lenght, body_w-0.1), (-boattail_lenght, 0.1)])
-    boattail = patches.Polygon(points, facecolor=alpha, edgecolor=edge_color)
+    boattail = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
     
     # Añadir todas las partes
     parts = [body, nose_cone, fin1, fin2, boattail]
@@ -67,5 +66,5 @@ def dibujar_cohete2(x_cm=3, y_cm=0.5, edge_color='b', alpha='0.8'):
     return fig, parts
 
 # Ejemplo de uso:
-#fig, parts = dibujarCohete()
-#plt.show()
+fig, parts = dibujar_cohete2()
+plt.show()
