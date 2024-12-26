28532ca5fea61b19eee42f09f079ee7eb456d66e
Meijix <nmejia30@ciencias.unam.mx>
2024-12-25 22:35:39 -0600

    Add latexify example

---
+++ Extras/Pruebas/latexify-example.py
@@ -0,0 +1,43 @@
+import latexify
+import math
+import numpy as np
+
+@latexify.expression
+def solve(a, b, c):
+    return (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+
+print(solve(1, 4, 3))  # Invoking the function works as expected.
+print(solve)  # Printing the function shows the underlying LaTeX source.
+solve  # Displays the expression.
+
+# Elif or nested else-if are unrolled.
+@latexify.function
+def der_gravedad_arrastre(t, state):
+    v = state[1]
+    if v == 0:
+        Drag = 0
+    else:
+        Drag = (D_mag/m) * (v**2) * np.sign(v)
+    
+    derivs = np.array((v, -g - Drag))
+    #print(derivs)
+    return derivs
+
+der_gravedad_arrastre
+print(der_gravedad_arrastre)
+
+
+# latexify.algorithmic generates an algorithmic environment instead of an equation.
+@latexify.algorithmic
+def der_gravedad_arrastre(t, state):
+    v = state[1]
+    if v == 0:
+        Drag = 0
+    else:
+        Drag = (D_mag/m) * (v**2) * np.sign(v)
+    
+    derivs = np.array((v, -g - Drag))
+    #print(derivs)
+    return derivs
+
+print(der_gravedad_arrastre)  # The function is displayed as an algorithm.
