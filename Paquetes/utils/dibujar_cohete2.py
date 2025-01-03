5990271aa2dc9baae88111c2411c70ef6f8f9fd3
Meijix <nmejia30@ciencias.unam.mx>
2025-01-02 20:35:17 -0600

    update drawing

--- Paquetes/utils/dibujar_cohete2.py
+++ Paquetes/utils/dibujar_cohete2.py
@@ -4,7 +4,7 @@ import matplotlib.patches as patches
 from matplotlib import transforms
 from matplotlib.animation import FuncAnimation
 
-def dibujar_cohete2(ax, angle=0, x_cm=0, y_cm=0, body_l=5, body_w=5/6, nose_l=5/4, fin_w1=5/4, fin_w2=5/3, fin_h=5/8, boattail_length=5/9, boat_rear=5/2):
+def dibujar_cohete2(ax, angle=0, x_cm=0, y_cm=0, body_l=3.3, body_w=0.16, nose_l=0.81, fin_tip=0.2, fin_root=0.3, fin_h=0.2, boattail_length=0.15, boat_rear=0.12):
     """
     Dibuja un cohete en un gráfico especificando su centro de gravedad (x_cm, y_cm),
     el ángulo de rotación, y el escalado del cohete.
@@ -12,38 +12,28 @@ def dibujar_cohete2(ax, angle=0, x_cm=0, y_cm=0, body_l=5, body_w=5/6, nose_l=5/
     # Colores
     color_cohete = 'midnightblue'
     color_borde = 'lavender'
-    
-    '''
-    # Dimensiones del cohete escaladas
-    body_l = long # Longitud del cuerpo
-    body_w = long / 6 # Ancho del cuerpo
-    nose_l = long / 4 # Longitud de la nariz
-    fin_w1 = long / 4 # Root chord de la aleta
-    fin_w2 = long / 3 # Tip chord de la aleta
-    fin_h = long / 8 # Altura de la aleta- span??
-    boattail_length = long / 9  # Longitud del boattail  
-    '''
     # Partes del cohete
     parts = []
     
+    # Cono de la nariz
+    points = np.array([(0, 0), (nose_l, body_w/2), (nose_l, -body_w / 2)])
+    nose_cone = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
+    parts.append(nose_cone)
+
     # Cuerpo
-    points = np.array([(0, 0), (0, body_w), (body_l, body_w), (body_l, 0)])
+    points = np.array([(nose_l, body_w/2), (nose_l, -body_w / 2), (nose_l+body_l, -body_w / 2),(nose_l+body_l, body_w/2), ])
     body = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
     parts.append(body)
     
-    # Cono de la nariz
-    points = np.array([(body_l, 0), (body_l, body_w), (body_l + nose_l, body_w / 2)])
-    nose_cone = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
-    parts.append(nose_cone)
     
     # Aletas
-    fin_d = (fin_w2 - fin_w1) / 2
-    points = np.array([(0, 0), (fin_d, fin_h), (fin_d + fin_w1, fin_h), (fin_w2, 0)])
+    inicio_aletas = nose_l + body_l
+    fin_d = (fin_root - fin_tip) / 2
+    # Puntos de la aleta superior
+    points = np.array([(inicio_aletas, body_w/2), (inicio_aletas-fin_root, body_w/2), (inicio_aletas-fin_root+fin_d, (body_w/2)+fin_h), (inicio_aletas-fin_d, (body_w/2)+fin_h)])
     
-    # Aleta superior
-    points1 = np.copy(points)
-    points1[:, 1] += body_w
-    fin1 = patches.Polygon(points1, facecolor=color_cohete, edgecolor=color_borde)
+
+    fin1 = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
     parts.append(fin1)
     
     # Aleta inferior
@@ -54,10 +44,10 @@ def dibujar_cohete2(ax, angle=0, x_cm=0, y_cm=0, body_l=5, body_w=5/6, nose_l=5/
     
     # Boattail
     points = np.array([
-        (0, -body_w/2), 
-        (0, body_w/2), 
-        (-boattail_length, -boat_rear/2), 
-        (-boattail_length, boat_rear/2)
+        (inicio_aletas, body_w/2),
+        (inicio_aletas, -body_w/2),
+        (inicio_aletas+boattail_length, -boat_rear/2),
+        (inicio_aletas+boattail_length, boat_rear/2)
     ])
     boattail = patches.Polygon(points, facecolor=color_cohete, edgecolor=color_borde)
     parts.append(boattail)
@@ -68,8 +58,8 @@ def dibujar_cohete2(ax, angle=0, x_cm=0, y_cm=0, body_l=5, body_w=5/6, nose_l=5/
     
     # Aplicar transformación inicial
     trans = (transforms.Affine2D()
-            .translate(-body_l / 2, -body_w / 2)  # Trasladar el cohete al origen relativo
-            .translate(x_cm, y_cm) 
+            #.translate(-body_l / 2, -body_w / 2)  # Trasladar el cohete al origen relativo
+            #.translate(x_cm, y_cm) 
             .rotate_deg(angle)      # Rotar alrededor del centro de gravedad    
             + ax.transData)
     for part in parts:
@@ -78,15 +68,15 @@ def dibujar_cohete2(ax, angle=0, x_cm=0, y_cm=0, body_l=5, body_w=5/6, nose_l=5/
     return parts
 
 # Función de animación
-def actualizar(frame, ax, parts, x_cm, y_cm, angle):
+def actualizar(ax, parts, x_cm, y_cm, angle):
     """
     Actualiza la rotación del cohete en cada cuadro.
     """
     # Rotación incremental en cada frame
     trans = (transforms.Affine2D()
-             .rotate_deg(angle)      # Rotar alrededor del centro de gravedad
-             .translate(x_cm, y_cm)  # Mover al centro de gravedad
-             + ax.transData)
+            .rotate_deg(angle)      # Rotar alrededor del centro de gravedad
+            .translate(x_cm, y_cm)  # Mover al centro de gravedad
+            + ax.transData)
     for part in parts:
         part.set_transform(trans)
     return parts
@@ -103,29 +93,31 @@ def on_click(event):
         ax.set_xlim(-20, 20)
         ax.set_ylim(-20, 20)
         ax.set_aspect('equal')
-        parts = dibujar_cohete2(ax, angle=0, x_cm=x_cm, y_cm=y_cm, long=5)
+        parts = dibujar_cohete2(ax, angle=0, x_cm=x_cm, y_cm=y_cm)
         anim = FuncAnimation(fig, actualizar, frames=np.arange(0, 360, 2), 
-                             fargs=(ax, parts, x_cm, y_cm, scale), interval=50)
+                            fargs=(ax, parts, x_cm, y_cm), interval=50)
         plt.draw()
 
 if __name__ == '__main__':
     # Configuración inicial del gráfico
     fig, ax = plt.subplots()
-    ax.set_xlim(-20, 20)
-    ax.set_ylim(-20, 20)
+    ax.set_xlim(0, 5)
+    ax.set_ylim(-0.5, 0.5)
     ax.set_aspect('equal')
+    #linea horizontal en y=0
+    ax.axhline(0, color='black', lw=1)
 
     # Variables globales
-    x_cm, y_cm = 0, 0  # Centro de gravedad inicial
+    x_cm, y_cm = 2, 0  # Centro de gravedad inicial
     scale = 1          # Escala inicial
-    parts = dibujar_cohete2(ax, angle=0, x_cm=x_cm, y_cm=y_cm, long=5)
-
+    parts = dibujar_cohete2(ax, angle=0, x_cm=x_cm, y_cm=y_cm)
+    '''
     # Crear animación inicial
     anim = FuncAnimation(fig, actualizar, frames=np.arange(0, 360, 2), 
                         fargs=(ax, parts, x_cm, y_cm, scale), interval=50)
 
     # Conectar evento de clic
     fig.canvas.mpl_connect('button_press_event', on_click)
-
+    '''
     # Mostrar el gráfico interactivo
     plt.show()
